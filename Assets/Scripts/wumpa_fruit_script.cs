eeaaa305b433b157bbfc5193c5f1ff75dc899889
Yasser Aboel-Saad <yasser.aboelsaad@gmail.com>
2016-12-29 17:29:11 +0100

    fixed jump and added apple collecting

---
+++ Assets/Scripts/wumpa_fruit_script.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wumpa_fruit_script : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter(Collider c){
+		Destroy (this.gameObject);
+	}
+}
