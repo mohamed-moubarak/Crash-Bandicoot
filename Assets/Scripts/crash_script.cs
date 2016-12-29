3b3b8d792f88bd6c8bed8c756e3632de70868b7a
Yasser Aboel-Saad <yasser.aboelsaad@gmail.com>
2016-12-29 05:02:57 +0100

    added crash to scene and added crash movment

--- Assets/Scripts/crash_script.cs
+++ Assets/Scripts/crash_script.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 
 public class crash_script : MonoBehaviour {
-	public int speed = 1;
+	public int WalkSpeed = 10;
+	public int TurnSpeed = 50;
+	public int JumpForce = 10;
 //	private Animation anim;
 
 	// Use this for initialization
@@ -14,21 +16,12 @@ public class crash_script : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		float v = Input.GetAxis ("Vertical");
-		transform.Translate (Vector3.forward * v * speed * Time.deltaTime);
-//		if (Input.GetButtonDown ("Jump")) {
-//			anim.Play ("crash_spin");
-//		}
-		float h = Input.GetAxis ("Horizontal");
-		if (Input.GetButtonDown ("Horizontal")) {
-			if (Input.GetAxis ("Horizontal") > 0) {
-				transform.Rotate (new Vector3 (0, 1, 0) * 90);
-				transform.Translate (Vector3.forward * speed * Time.deltaTime);
-			} else {
-				transform.Rotate (new Vector3 (0, 1, 0) * -90);
-				transform.Translate (Vector3.forward * speed * Time.deltaTime);
-			}
-
+		transform.Translate (Vector3.forward * v * WalkSpeed * Time.deltaTime);
+		Rigidbody rb = GetComponent<Rigidbody> ();
+		if (Input.GetButtonDown ("Jump")) {
+			rb.AddForce (new Vector3 (0, JumpForce, 0), ForceMode.Impulse);
 		}
-
+		float h = Input.GetAxis ("Horizontal");
+		transform.Rotate (new Vector3 (0, 1, 0) * h*Time.deltaTime*TurnSpeed);
 	}
 }
